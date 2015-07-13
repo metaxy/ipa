aa9fa4cebcec2369b01acdab355c7c0161eccd6d
walger@TU-BERLIN.DE <walger@TU-BERLIN.DE@a40b4ca5-01ff-4691-9e9b-37a67ebd2320>
2015-07-13 09:52:26 +0000

    stuff
    git-svn-id: https://svn.kbs.tu-berlin.de/svn/ipa-sose15-5@129 a40b4ca5-01ff-4691-9e9b-37a67ebd2320

--- src/client/src/app/user/user.controller.js
+++ src/client/src/app/user/user.controller.js
@@ -2,11 +2,12 @@
 export default function UserCtrl($rootScope, $http, ApiUrl) {
 
   $rootScope.siteTitle = "Benutzer";
-  this.reload();
 
   this.reload = () => {
-    this.users = $http.get(ApiUrl+'list_users');
+    this.users = $http.get(ApiUrl+'/list_users');
   }
+
+  this.reload();
   this.delete = (user_name) => {
     $http.post(ApiUrl+'/delete_account', {name : user_name})
       .success(() => this.reload())
