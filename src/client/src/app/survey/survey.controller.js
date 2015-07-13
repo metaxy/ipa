83a10b376f231ab7a192e18cf8020a940fbb3886
rbermudez@TU-BERLIN.DE <rbermudez@TU-BERLIN.DE@a40b4ca5-01ff-4691-9e9b-37a67ebd2320>
2015-07-13 15:41:54 +0000

    Button to close survey hinzugefügt
    git-svn-id: https://svn.kbs.tu-berlin.de/svn/ipa-sose15-5@148 a40b4ca5-01ff-4691-9e9b-37a67ebd2320

--- src/client/src/app/survey/survey.controller.js
+++ src/client/src/app/survey/survey.controller.js
@@ -7,8 +7,8 @@ export default function SurveyCtrl($stateParams,$rootScope, $http, Shout, ApiUrl
     });
   */
   this.room = $stateParams.roomId;
+  $rootScope.siteTitle = "Umfragen in " + this.room;
 
-  //todo: this.surveys = Room.survey({id: $stateParams.roomId});
   $http.get(ApiUrl+'/r/'+this.room)
   .success((data) => {
       this.surveys = data.surveys;
@@ -39,33 +39,17 @@ export default function SurveyCtrl($stateParams,$rootScope, $http, Shout, ApiUrl
 		.error((err) => {
       Shout.error('Could not create survey');
     })
-		
-    /*Room.survey.create({id:$stateParams.roomId}, survey).$promise.then((data) => {
-      this.options.forEach((d) => {
-        Survey.surveyOptions.create({id: data.id}, d);
-      });
-      this.update();
-      this.visible_create_from = false;
-    });*/
   }
 
   this.delete = (survey_id) => {
    //todo: Survey.deleteById({id: survey_id}).$promise.then(() => this.update());
-      $http.post(ApiUrl+'/r/'+this.room+'/s/'+survey_id+'/delete')
-      .success((data) => {
-        console.log('Survey closed');
-        this.update();
-      })
-      .error((err) => {
-        Shout.error('Could not delete survey');
-      })
+      
   }
 
   this.close = (survey_id) => {
     $http.post(ApiUrl+'/r/'+this.room+'/s/'+survey_id+'/close')
     .success((data) => {
       console.log('Survey closed');
-      this.update();
     })
     .error((err) => {
       Shout.error('Could not delete survey');
@@ -73,7 +57,6 @@ export default function SurveyCtrl($stateParams,$rootScope, $http, Shout, ApiUrl
   }
 
   this.update = () => {
-    //todo: Room.survey({id: $stateParams.roomId}).$promise.then((data) => this.surveys = data);
     $state.go($state.current, {}, {reload: true});
   }
 }
