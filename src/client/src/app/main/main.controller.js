c784989a8838e726d3fde3635a58f975cba8980b
kristianmitk@TU-BERLIN.DE <kristianmitk@TU-BERLIN.DE@a40b4ca5-01ff-4691-9e9b-37a67ebd2320>
2015-07-14 15:10:30 +0000

    assign_role works, create role/account works, create_room gives a 500 response - after a refresh the room is created, edit_role gives a 500 response as well - even the request object is a correct list_permission
    git-svn-id: https://svn.kbs.tu-berlin.de/svn/ipa-sose15-5@165 a40b4ca5-01ff-4691-9e9b-37a67ebd2320

--- src/client/src/app/main/main.controller.js
+++ src/client/src/app/main/main.controller.js
@@ -2,6 +2,10 @@
 import _ from 'lodash';
 export default function MainCtrl($state,$rootScope, Shout, $http, ApiUrl) {
 
+
+  this.optionalRights = ["create_question", "join_lecture", "vote_tempo", "vote_question", "vote_survey", "manage_lecture", "create_survey", "create_room", "view_tempo", "close_survey", "delete_question", "create_account", "delete_account", "assign_role", "create_role", "edit_role", "delete_role", "list_roles", "list_users"];
+
+
   $rootScope.siteTitle = "Start";
 
   $http.get(ApiUrl+'/list_rooms')
@@ -12,17 +16,23 @@ export default function MainCtrl($state,$rootScope, Shout, $http, ApiUrl) {
       Shout.error("Could not get rooms");
   });
 
+  $http.get(ApiUrl+'/list_users')
+    .success((data) => {
+    this.all_users = data.users;
+  })
+  .error(() => {
+    Shout.error("Could not get users");
+  });
+
   $http.get(ApiUrl+'/list_roles')
   .success((data) => {
       this.all_roles = data.roles;
    })
-  .error(() => {
+  .error((err) => {
       Shout.error("Could not get roles");
+      console.log(err);
   });
-  this.all_rooms = $http.get(ApiUrl+'/list_rooms');
-  this.optionalRights = ["create_question", "join_lecture", "vote_tempo", "vote_question", "vote_survey", "manage_lecture", "create_survey", "create_room", "view_tempo", "close_survey", "delete_question", "create_account", "delete_account", "assign_role", "create_role", "edit_role", "delete_role", "list_roles", "list_users"];
-  this.all_roles = $http.get(ApiUrl+'/list_roles');
-  this.all_users = $http.get(ApiUrl+'/list_users');
+
 
   this.joinRoom = (room_name, room_password) => {
     if(_.isUndefined(room_password)) {
@@ -41,8 +51,8 @@ export default function MainCtrl($state,$rootScope, Shout, $http, ApiUrl) {
   this.createRoom = (room_name, room_passkey) => {
     $http.post(ApiUrl+'/create_room', {name : room_name, passkey: room_passkey})
       .success((data) => {
-        console.log('Room created', data);
-        $state.go('room', {roomId: data.name});
+        Shout.success('Room created');
+        $state.go('room', {roomName: data.name});
       })
       .error((err) => {
         Shout.error('Could not create room');
@@ -53,6 +63,7 @@ export default function MainCtrl($state,$rootScope, Shout, $http, ApiUrl) {
     $http.post(ApiUrl+'/create_account', {name : account_name, password: account_password})
       .success((data) => {
         console.log('Account created', data);
+        Shout.success('Account created');
         $state.go('user');
       })
       .error((err) => {
@@ -63,21 +74,53 @@ export default function MainCtrl($state,$rootScope, Shout, $http, ApiUrl) {
   this.createRole = (role_name) => {
     $http.post(ApiUrl+'/create_role', {role : role_name})
       .success((data) => {
-        console.log('Role created', data);
+        Shout.success('Role created');
       })
       .error((err) => {
         Shout.error('Could not create role');
       });
   }
 
-  this.editRole = (role_name) => {
-    /*$http.post(ApiUrl+'/role/'+role_name, {perms : })
-      .success((data) => {
+  this.assignRole = (user_name,role_name) => {
+    if(_.isUndefined(role_name)) {
+      Shout.error("Error: Choose a rolename");
+      return;
+    }
+    if(_.isUndefined(user_name)) {
+      Shout.error("Error: Choose a username");
+      return;
+    }
 
+    $http.post(ApiUrl+'/assign_role', {name : user_name,role: role_name})
+      .success((data) => {
+        Shout.success('Role successfully asigned');
       })
       .error((err) => {
+        Shout.error(err);
+      });
+  }
 
-      });*/
-  } 
+  this.editRole = (role_name,new_list_permission) => {
+    if(_.isUndefined(role_name)) {
+      Shout.error("Error: Choose a rolename");
+      return;
+    }
+    $http.post(ApiUrl+'/role/'+role_name, new_list_permission)
+      .success((data) => {
+        Shout.success('Role successfully edited');
+      })
+      .error((err) => {
+        Shout.error(err);
+      });
+  }
 
+    $rootScope.selectedRights = [];
+    $rootScope.toggle = function (item, list) {
+      var idx = list.indexOf(item);
+      if (idx > -1) list.splice(idx, 1);
+      else list.push(item);
+    };
+    $rootScope.exists = function (item, list) {
+      return list.indexOf(item) > -1;
+    };
 }
