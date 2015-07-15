ce0b1e98fd4ee4f994e9779ad4e343ca1d39a47a
walger@TU-BERLIN.DE <walger@TU-BERLIN.DE@a40b4ca5-01ff-4691-9e9b-37a67ebd2320>
2015-07-15 15:38:31 +0000

    add e2e tests
    git-svn-id: https://svn.kbs.tu-berlin.de/svn/ipa-sose15-5@174 a40b4ca5-01ff-4691-9e9b-37a67ebd2320

---
+++ src/client/e2e/home.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('test the home', function () {
+  beforeEach(function () {
+    browser.get('#/');
+  });
+
+
+  xit('it should have a edit button', function () {
+  });
+
+});
